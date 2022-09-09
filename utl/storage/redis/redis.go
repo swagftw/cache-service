7d6f3bec9404f0815a3e1d111d4f92614fb90788
Swapnil Nakade <swapnil@obmondo.com>
2022-09-10 00:42:33 +0530

    doc: adding service doc

--- utl/storage/redis/redis.go
+++ utl/storage/redis/redis.go
@@ -1,18 +1,23 @@
 package redis
 
 import (
-	"github.com/go-redis/redis/v9"
+	"os"
 	"strconv"
 	"time"
 
+	"github.com/go-redis/redis/v9"
 	"github.com/swagftw/cache-service/utl/config"
 )
 
 // InitRedisDB initializes a new redis client.
 func InitRedisDB(cfg *config.Config) *redis.Client {
 	port := strconv.Itoa(cfg.Redis.Port)
+	host, ok := os.LookupEnv("REDIS_HOST")
+	if !ok {
+		host = "localhost"
+	}
 	rdb := redis.NewClient(&redis.Options{ //nolint:exhaustivestruct
-		Addr:         ":" + port,
+		Addr:         host + ":" + port,
 		DB:           0, // use default DB
 		ReadTimeout:  time.Duration(cfg.Redis.Timeout) * time.Second,
 		WriteTimeout: time.Duration(cfg.Redis.Timeout) * time.Second,
