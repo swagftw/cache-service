16dc993742aba427762447913eb6ec033425139c
Swapnil Nakade <swapnil@obmondo.com>
2022-09-09 23:01:27 +0530

    feat: adding client support user cache

--- cmd/rpc_client/main.go
+++ cmd/rpc_client/main.go
@@ -33,15 +33,33 @@ func main() {
 	client := cacheService.NewCacheServiceClient(connection)
 
 	if args[0] == "get" && len(args) == 2 {
-		Get(client, args[1])
+		get(client, args[1])
 	} else if args[0] == "set" && len(args) == 3 {
-		Set(client, args[1], args[2])
+		set(client, args[1], args[2])
+	} else if args[0] == "getuser" && len(args) == 3 {
+		rollNum, err := strconv.ParseInt(args[2], 10, 64)
+		if err != nil {
+			log.Printf("Failed to parse roll number: %v\n", err)
+
+			return
+		}
+
+		getUser(client, args[1], rollNum)
+	} else if args[0] == "setuser" && len(args) == 4 {
+		rollNum, err := strconv.ParseInt(args[3], 10, 64)
+		if err != nil {
+			log.Printf("Failed to parse roll number: %v\n", err)
+
+			return
+		}
+
+		setUser(client, args[1], args[2], rollNum)
 	} else {
 		log.Printf("Invalid command: %s\n", args[0])
 	}
 }
 
-func Get(client cacheService.CacheServiceClient, key string) {
+func get(client cacheService.CacheServiceClient, key string) {
 	response, err := client.GetValue(context.TODO(), &cacheService.GetRequest{Key: key})
 	if err != nil {
 		log.Printf("Failed to get value: %v\n", err)
@@ -52,13 +70,45 @@ func Get(client cacheService.CacheServiceClient, key string) {
 	log.Printf("Value: %s\n", response.Value)
 }
 
-func Set(client cacheService.CacheServiceClient, key string, value string) {
+func set(client cacheService.CacheServiceClient, key string, value string) {
 	_, err := client.SetValue(context.TODO(), &cacheService.SetRequest{Key: key, Value: value})
 	if err != nil {
 		log.Printf("Failed to set value: %v\n", err)
-		
+
 		return
 	}
 
 	log.Println("OK")
+}
+
+func setUser(client cacheService.CacheServiceClient, name, class string, rollNum int64) {
+	resp, err := client.SetUser(context.TODO(), &cacheService.User{
+		Name:     name,
+		Class:    class,
+		RollNum:  rollNum,
+		Metadata: nil,
+	})
+
+	if err != nil {
+		log.Printf("Failed to set user: %v\n", err)
+
+		return
+	}
+
+	log.Println(resp)
+}
+
+func getUser(client cacheService.CacheServiceClient, name string, rollNum int64) {
+	resp, err := client.GetUser(context.TODO(), &cacheService.GetUserRequest{
+		Name:    name,
+		RollNum: rollNum,
+	})
+
+	if err != nil {
+		log.Printf("Failed to get user: %v\n", err)
+
+		return
+	}
+
+	log.Println(resp)
 }
